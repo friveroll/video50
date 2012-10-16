abb0c6d6b91dafb916a138ca19fa0169bb583ac9
Joseph Ong <ong.joseph.y@gmail.com>
2012-10-15 23:43:19 -0400

    fixed srt menu displacement

--- js/cs50.video.js
+++ js/cs50.video.js
@@ -162,7 +162,7 @@ CS50.Video = function(options) {
                             <button class="btn dropdown-toggle" data-toggle="dropdown"> \
                                 <span class="caret"></span> \
                             </button> \
-                            <ul class="dropdown-menu transcript-lang"> \
+                            <ul class="dropdown-menu transcript-lang pull-right"> \
                                 <% for (var i in srt) { %> \
                                     <li> \
                                         <a href="#" data-lang="<%= i %>"><%= CS50.Video.Languages[i] %></a> \
